40f1055135d7dc4a41e9b57a0e153f4f19e09bd6
Vijayashre <vkaushiwa@gmail.com>
2023-12-12 22:18:52 +0530

    basic js

---
+++ num_maths.js
@@ -0,0 +1,4 @@
+const scrore =  400
+
+const balance = new Number(100)
+console.log(balance);
---
+++ string.js
@@ -0,0 +1,37 @@
+const name = "Shree"
+
+const repocount = 50
+
+
+// console.log(`Hello my name is  ${name} and my repo count is ${repocount}`);      // using back stick here 
+
+const gameName = new String('Vijaya-shree')
+
+// console.log(gameName[0]);
+
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+
+// console.log(gameName.toUpperCase());
+
+// console.log(gameName.charAt(2));
+
+// console.log(gameName.indexOf('a'));
+
+const newString = gameName.substring(0, 3)
+//console.log(newString);
+
+const anotherString = gameName.slice(-4, 4)
+//console.log(anotherString);
+
+const newString1 = "   Kaushiwa   "
+// console.log(newString1);
+// console.log(newString1.trim());
+
+const url = "https://github.com/Vijayashre/javascript-code"
+// console.log(url.replace("Vijayashre",'kaushiwa'));
+// console.log(url.includes('VijayshreeK'));
+
+console.log(gameName.split('2'));
+
